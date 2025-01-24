830ba889ee9bb534202c2e936aab73e4e8330878
Xelleet <ererenyeager@yandex.ru>
2025-01-24 21:11:22 +0300

    Added forms

---
+++ vk/main/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from . import models
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = models.Post
+        fields = ['title', 'content']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = models.Comment
+        fields = ['content']
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    class Meta:
+        model = models.User
+        fields = ['username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise ValidationError("Passwords do not match!")
+        return cleaned_data
+
+class LoginForm(forms.Form):#
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
