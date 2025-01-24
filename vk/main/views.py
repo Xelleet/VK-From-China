500082c08574f755b378e46e667944c250d9fb5e
Xelleet <ererenyeager@yandex.ru>
2025-01-24 21:28:49 +0300

    Views and urls now implement and render all the things that we have

--- vk/main/views.py
+++ vk/main/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, logout, login
+from .models import User, Post, Comment
+from .forms import RegisterForm, LoginForm, PostForm, CommentForm
 
+@login_required
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'user': request.user})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])  # Hash the password
+            user.save()
+            login(request, user)  # Log in the new user
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
